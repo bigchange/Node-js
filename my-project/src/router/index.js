e2dcf9e9f552278aecbe9bf63dfe2ff4861364c7
shanghai-Jerry <cjyou20110920sspu@163.com>
2017-07-24 14:27:45 +0800

    new dont want to continue but when it necessary

--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Demo from '@/components/Demo'
+import Table from '@/components/Table'
 
 Vue.use(Router)
 
@@ -13,9 +14,9 @@ export default new Router({
       component: Hello
     },
     {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
+      path: '/table',
+      name: 'Table',
+      component: Table
     },
     {
       path: '/demo',
